936084b75f34da476e91758aae9cb5562decf629
Lionel Mace <lionel.mace@gmail.com>
2023-06-22 09:18:44 +0300

    added sm

---
+++ security-secrets-manager.tf
@@ -0,0 +1,24 @@
+##############################################################################
+## Secrets Manager
+##############################################################################
+resource "ibm_resource_instance" "secrets-manager" {
+  name              = format("%s-%s", local.basename, "secrets-manager")
+  service           = "secrets-manager"
+  plan              = "trial"
+  location          = var.region
+  resource_group_id = ibm_resource_group.group.id
+  tags              = var.tags
+  service_endpoints = "private"
+}
+
+resource "ibm_sm_secret_group" "sm_secret_group"{
+  instance_id   = ibm_resource_instance.secrets-manager.guid
+  region        = var.region
+  name          = format("%s-%s", local.basename, "sm-group")
+  description   = "Secret Group"
+}
+
+output "secrets-manager-crn" {
+  description = "The CRN of the Secrets Manager instance"
+  value       = ibm_resource_instance.secrets-manager.id
+}
---
+++ variables.tf
@@ -0,0 +1,36 @@
+##############################################################################
+# Account Variables
+##############################################################################
+
+variable "prefix" {
+  type        = string
+  default     = ""
+  description = "A prefix for all resources to be created. If none provided a random prefix will be created"
+}
+
+resource "random_string" "random" {
+  count = var.prefix == "" ? 1 : 0
+
+  length  = 6
+  special = false
+}
+
+locals {
+  basename = lower(var.prefix == "" ? "cn-${random_string.random.0.result}" : var.prefix)
+}
+
+resource "ibm_resource_group" "group" {
+  name = "${local.basename}-group"
+  tags = var.tags
+}
+
+variable "region" {
+  description = "IBM Cloud region where all resources will be provisioned (e.g. eu-de)"
+  default     = "eu-de"
+}
+
+variable "tags" {
+  description = "List of Tags"
+  type        = list(string)
+  default     = ["tf", "cn"]
+}
